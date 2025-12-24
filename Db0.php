0a3a023104266f6c810e4ce809e669f244a41445
Ariel Toure <arielz360007@gmail.com>
2025-12-24 09:16:23 +0000

    Create Db0.php

---
+++ Db0.php
@@ -0,0 +1,16 @@
+<?php
+//Coordonnées du serveur Sql
+$dbname="exo";
+$host="127.0.1.0";
+$port=3300;
+$username="root";
+$password="";
+
+//Connexion au serveur Php
+try{
+$connect= new PDO("mysql:dbname=$dbname; host=$host; port=$port; charset=utf8mb4" $username, $password);
+$connect-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+  echo "Erreur de Connexion." . $e->getMessage();
+}
+?>
